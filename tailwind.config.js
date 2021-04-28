2747015c3629b053494ca4765b55ecf5a9e67106
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-04-28 12:17:20 +0200

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        primary: {
+          100: "#EFF6F4",
+          300: "#9EBDB4",
+          500: "#045C61",
+        },
+        secondary: {
+          100: "#F5F6FA",
+          300: "#FAF4E2"
+        },
+        black: {
+          DEFAULT: "#282828"
+        }
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
