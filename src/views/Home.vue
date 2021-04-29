056882a77017370360a44b53d377300de8decc53
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-04-29 10:48:34 +0200

    add footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -43,17 +43,13 @@
                     width="436.057" height="469.847"
                     viewBox="0 0 436.057 469.847">
                   <g transform="translate(-16.896)">
-                    <path
-                        d="M270.336,234.965,34.39,462.165A40.146,40.146,0,0,1,16.9,428.672V41.258A40.146,40.146,0,0,1,34.39,7.765Z"
+                    <path d="M270.336,234.965,34.39,462.165A40.146,40.146,0,0,1,16.9,428.672V41.258A40.146,40.146,0,0,1,34.39,7.765Z"
                         fill="#2196f3"/>
-                    <path
-                        d="M352.9,155.6l-82.56,79.36L34.39,7.765a31.446,31.446,0,0,1,2.773-1.92A40.363,40.363,0,0,1,77.91,5.2Z"
+                    <path d="M352.9,155.6l-82.56,79.36L34.39,7.765a31.446,31.446,0,0,1,2.773-1.92A40.363,40.363,0,0,1,77.91,5.2Z"
                         fill="#4caf50"/>
-                    <path
-                        d="M452.95,234.965a40.791,40.791,0,0,1-21.333,36.267L352.9,314.325l-82.56-79.36L352.9,155.6l78.72,43.093A40.791,40.791,0,0,1,452.95,234.965Z"
+                    <path d="M452.95,234.965a40.791,40.791,0,0,1-21.333,36.267L352.9,314.325l-82.56-79.36L352.9,155.6l78.72,43.093A40.791,40.791,0,0,1,452.95,234.965Z"
                         fill="#ffc107"/>
-                    <path
-                        d="M352.9,314.325,77.91,464.725a40.9,40.9,0,0,1-40.747-.64,31.44,31.44,0,0,1-2.773-1.92l235.947-227.2Z"
+                    <path d="M352.9,314.325,77.91,464.725a40.9,40.9,0,0,1-40.747-.64,31.44,31.44,0,0,1-2.773-1.92l235.947-227.2Z"
                         fill="#f44336"/>
                   </g>
                 </svg>
@@ -75,7 +71,7 @@
   </div><!-- end hero -->
 
   <!-- BLOG -->
-  <div class="template">
+  <div class="template py-16">
     <section class="text-left px-8">
       <h2 class="uppercase leading-none font-black text-primary-500 text-5xl mb-2">Le blog Belhom</h2>
       <h3 class="text-primary-300 text-3xl mb-5">Ensemble contre le gaspillage alimentaire<br/>Apprenez, comprenez,
@@ -83,8 +79,7 @@
         le changement !</h3>
     </section>
 
-    <section class="pb-4 pb-md-5">
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6 container m-auto">
+    <section class="grid grid-cols-1 md:grid-cols-3 gap-16 max-w-6xl m-auto py-4">
         <div class="my-3 col-md-4 col-12">
           <a href="/fr/blog/utiliser-feuille-artichaut" class="">
             <div class="blog-card text-xl h-full bg-white shadow-xl rounded-3xl">
@@ -121,18 +116,18 @@
             </div>
           </a>
         </div>
-      </div>
     </section>
 
   </div>
   </body>
+  <Footer/>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import Footer from "@/components/Footer";
 export default {
   name: 'Home',
-  components: {}
+  components: {Footer}
 }
 </script>
