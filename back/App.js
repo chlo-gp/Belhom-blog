966599cac25e1b97fbff96c60c5f7dc555e90e33
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-06-09 12:01:16 +0200

    fix build

--- back/App.js
+++ back/App.js
@@ -34,6 +34,6 @@ app.get("/blog/:id", (req, res) => {
     res.json(article);
 });
 
-const port = 5000
+const port = process.env.PORT || 5000
 app.listen(port)
 console.log(`app is listening on port : ${port}`)
