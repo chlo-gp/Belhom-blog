7423ba36599471becc9b630a95ab6cd3f68277e4
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-06-10 14:30:58 +0200

    fix get

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -121,7 +121,7 @@ export default {
     fetchArticles() {
       axios
           .get('https://belhom-website.herokuapp.com/blog')
-          .then(response => (this.results.push.apply(this.results, response.data)))
+          .then(response => (this.results.push.apply(this.results, [response.data])))
           .catch(error => {
             console.log(error)
             this.errored = true
