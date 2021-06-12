11fd5bfd0d2256f21c00d6168881b63bfaf0ef52
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-06-12 15:49:09 +0200

    feat:add new controllers

---
+++ back/controllers/articles.controller.js
@@ -0,0 +1,138 @@
+const db = require("../models");
+const Articles = db.articles;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Article
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+      res.status(400).send({
+        message: "Title can not be empty!"
+      });
+      return;
+    } else if (!req.body.category) {
+        res.status(400).send({
+          message: "Category can not be empty!"
+        });
+        return;
+      }
+  
+    // Create an Article
+    const article = {
+      title: req.body.title,
+      category: req.body.category,
+      lead: req.body.lead,
+      content: req.body.content,
+      desc: req.body.desc,
+      thumbnail: req.body.thumbnail,
+    };
+  
+    // Save Article in the database
+    Articles.create(article)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Article."
+        });
+      });
+  };
+
+// Retrieve all Articles from the database.
+exports.findAll = (req, res) => {
+  
+    Articles.findAll()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving articles."
+        });
+      });
+  };
+
+// Find a single Article with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    Articles.findByPk(id)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving Article with id=" + id
+        });
+      });
+  };
+
+// Update a Article by the id in the request
+exports.update = (req, res) => {
+    const id = req.params.id;
+  
+    Articles.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Article was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update Article with id=${id}. Maybe Article was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating Article with id=" + id
+        });
+      });
+  };
+
+// Delete a Article with the specified id in the request
+exports.delete = (req, res) => {
+    const id = req.params.id;
+  
+    Articles.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Article was deleted successfully!"
+          });
+        } else {
+          res.send({
+            message: `Cannot delete Article with id=${id}. Maybe Article was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete Article with id=" + id
+        });
+      });
+  };
+
+// Delete all Articles from the database.
+exports.deleteAll = (req, res) => {
+    Articles.destroy({
+      where: {},
+      truncate: false
+    })
+      .then(nums => {
+        res.send({ message: `${nums} Articles were deleted successfully!` });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while removing all articles."
+        });
+      });
+  };
---
+++ back/controllers/products.controller.js
@@ -0,0 +1,148 @@
+const db = require("../models");
+const Products = db.product;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Product
+exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.name) {
+      res.status(400).send({
+        message: "Name can not be empty!"
+      });
+      return;
+    } else if (!req.body.category) {
+        res.status(400).send({
+          message: "Category can not be empty!"
+        });
+        return;
+      } else if (!req.body.brand) {
+        res.status(400).send({
+          message: "Brand can not be empty!"
+        });
+        return;
+      } else if (!req.body.price) {
+        res.status(400).send({
+          message: "Price can not be empty!"
+        });
+        return;
+      }
+  
+    // Create a Product
+    const product = {
+      name: req.body.name,
+      category: req.body.category,
+      brand: req.body.brand,
+      price: req.body.price,
+      desc: req.body.desc,
+      thumbnail: req.body.thumbnail,
+    };
+  
+    // Save Product in the database
+    Products.create(product)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while creating the Product."
+        });
+      });
+  };
+
+// Retrieve all Products from the database.
+exports.findAll = (req, res) => {
+  
+    Products.findAll()
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving products."
+        });
+      });
+  };
+
+// Find a single product with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+  
+    Products.findByPk(id)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error retrieving product with id=" + id
+        });
+      });
+  };
+
+// Update a product by the id in the request
+exports.update = (req, res) => {
+    const id = req.params.id;
+  
+    Products.update(req.body, {
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Product was updated successfully."
+          });
+        } else {
+          res.send({
+            message: `Cannot update product with id=${id}. Maybe product was not found or req.body is empty!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Error updating product with id=" + id
+        });
+      });
+  };
+
+// Delete a product with the specified id in the request
+exports.delete = (req, res) => {
+    const id = req.params.id;
+  
+    Products.destroy({
+      where: { id: id }
+    })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Product was deleted successfully!"
+          });
+        } else {
+          res.send({
+            message: `Cannot delete Product with id=${id}. Maybe Product was not found!`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: "Could not delete product with id=" + id
+        });
+      });
+  };
+
+// Delete all products from the database.
+exports.deleteAll = (req, res) => {
+    Products.destroy({
+      where: {},
+      truncate: false
+    })
+      .then(nums => {
+        res.send({ message: `${nums} Products were deleted successfully!` });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while removing all products."
+        });
+      });
+  };
