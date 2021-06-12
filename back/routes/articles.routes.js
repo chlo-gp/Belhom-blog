3b88fe12a636021d37f8007a298845b821adc450
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-06-12 15:49:31 +0200

    feat:add routes

---
+++ back/routes/articles.routes.js
@@ -0,0 +1,25 @@
+module.exports = app => {
+    const articles = require("../controllers/articles.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new Article
+    router.post("/", articles.create);
+  
+    // Retrieve all Articles
+    router.get("/", articles.findAll);
+  
+    // Retrieve a single Article with id
+    router.get("/:id", articles.findOne);
+  
+    // Update a Article with id
+    router.put("/:id", articles.update);
+  
+    // Delete a Article with id
+    router.delete("/:id", articles.delete);
+  
+    // Delete all Articles
+    router.delete("/", articles.deleteAll);
+  
+    app.use('/blog', router);
+  };
