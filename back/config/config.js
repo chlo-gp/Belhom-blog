57381dbd8d4b3910374b42e64c1b55c7551ecfe5
Chloé Gallego <chloe.gallego1501@gmail.com>
2021-07-08 10:40:09 +0200

    feat:update db infos

--- back/config/config.js
+++ back/config/config.js
@@ -1,19 +1,13 @@
 module.exports = {
-   "development": {
-     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "belhom",
+     HOST: "us-cdbr-iron-east-02.cleardb.net",
+    USER: "b7e2437887xxxa",
+    PASSWORD: "0200xxx6",
+    DB: "heroku_7643ec736354xxx",
     dialect: "mysql",
     pool: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000
-    }
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
     }
   };
