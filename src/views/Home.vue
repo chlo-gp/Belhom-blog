3b04becd761893977249171986fdd1cb5f6badd0
Chloé Gallego <chloe.gallego1501@gmail.com>
2022-03-02 16:01:03 +0100

    feat:add typed js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -200,13 +200,11 @@
 <script>
 // @ is an alias to /src
 import Footer from "@/components/Footer";
-import Vue from 'vue'
 import { VueTypedJs } from 'vue-typed-js'
  
-Vue.use(VueTypedJs)
 
 export default {
   name: "Home",
-  components: {Footer},
+  components: {Footer, VueTypedJs},
 }
 </script>
