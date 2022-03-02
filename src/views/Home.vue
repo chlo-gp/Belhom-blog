c883c390b97ded1f19cbde95ce2d5ff6719cd937
Chloé Gallego <chloe.gallego1501@gmail.com>
2022-03-02 15:30:46 +0100

    feat:add blob

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -65,7 +65,7 @@
         </div>
         <!-- hero image -->
         <div class="hero-image col-span-5 relative top-0 left-0">
-          <img src="@/assets/blob.svg" class="relative top-0 left-8">
+          <img src="@/assets/blob.svg" class="relative top-0" style="left:32px">
           <img src="@/assets/mokup.png" class="absolute left-0 top-0">
         </div>
       </div>
