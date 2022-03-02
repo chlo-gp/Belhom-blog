706f9651b4e56b7e4152d24b4baffc0e235c0307
Chloé Gallego <chloe.gallego1501@gmail.com>
2022-03-02 15:52:56 +0100

    feat:add typed js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
-import VueTypedJs from 'vue-typed-js'
+import Vue from 'vue'
+import { VueTypedJs } from 'vue-typed-js'
+ 
+Vue.use(VueTypedJs)
 
-
-createApp(App).use(router).use(VueTypedJs).mount('#app')
+createApp(App).use(router).mount('#app')
